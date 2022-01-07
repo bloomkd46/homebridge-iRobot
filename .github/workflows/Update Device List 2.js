b3f0e9a7654e813ce6ba895f8ccae57a71afaa8a
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2022-01-07 09:44:46 -0800

    Update Update Device List 2.js

--- .github/workflows/Update Device List 2.js
+++ .github/workflows/Update Device List 2.js
@@ -1,3 +1,4 @@
+/*
 const fs = require('fs');
 const eventPayload = require(process.env.GITHUB_EVENT_PATH);
 const order = require('./order.json');
@@ -10,3 +11,16 @@ const amount = parseInt(count.slice(0,3), 10) || 1;
 const content = `1. [@${user}](https://github.com/${user}) orders ${amount} ${size} ${flavour} pizza with ${toppings}\n`;
 
 fs.appendFileSync('README.md', content);
+*/
+const eventPayload = require(process.env.GITHUB_EVENT_PATH);
+const device = require('./device.json');
+const [roomba_model, supported] = Object.values(device);
+var fs = require('fs');
+var lineNumber = 36;
+var data = fs.readFileSync('readme.md').toString().split("\n");
+data.splice(lineNumber, 0, ("| " + roomba_model + " | " + supported + " | " + user + " |"));
+var text = data.join("\n");
+
+fs.writeFile('readme.md', text, function (err) {
+  if (err) return console.log(err);
+});
