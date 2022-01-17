10e7806cd064f47c8a34a17c1b5f45ba92c42b9f
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2022-01-17 15:31:20 -0800

    Delete Update Device List.js

--- .github/workflows/Update Device List.js
+++
@@ -1,9 +0,0 @@
-const core = require('@actions/core');
-const issue = core.getMultiLineInput('issue', {"required": true});
-const model = issue.split('\n')[3];
-const supported = issue.split('\n')[7];
-if (supported === "Yes" || supported === "No") {
-    const tableRow = "| " + model + " | " + supported + " |";
-    const readme = fs.readFileSync('Readme.md').split('\n');
-    readme.splice(36, 0, tableRow);
-}
