471bca7c0b19525f13a084477548582772394765
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2022-01-17 15:32:29 -0800

    Update add-model.js

--- .github/workflows/add-model.js
+++ .github/workflows/add-model.js
@@ -1,17 +1,3 @@
-/*
-const fs = require('fs');
-const eventPayload = require(process.env.GITHUB_EVENT_PATH);
-const order = require('./order.json');
-
-const user = eventPayload.sender.login;
-const [flavour, size, toppings, count] = Object.values(order);
-
-const amount = parseInt(count.slice(0,3), 10) || 1;
-
-const content = `1. [@${user}](https://github.com/${user}) orders ${amount} ${size} ${flavour} pizza with ${toppings}\n`;
-
-fs.appendFileSync('README.md', content);
-*/
 const eventPayload = require(process.env.GITHUB_EVENT_PATH);
 const device = require('../../device.json');
 const user = eventPayload.sender.login;
@@ -19,7 +5,7 @@ const [roomba_model, supported] = Object.values(device);
 var fs = require('fs');
 var lineNumber = 36;
 var data = fs.readFileSync('README.md').toString().split("\n");
-data.splice(lineNumber, 0, ("| " + roomba_model + " | " + supported + " | @" + user + " |"));
+data.splice(lineNumber, 0, ("| " + roomba_model + " | " + supported + " | [" + user + "](https://github.com/" + user + ") |"));
 var text = data.join("\n");
 
 fs.writeFile('README.md', text, function (err) {
