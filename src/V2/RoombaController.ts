7778edd60605ae9b0725d373f85266751fccdd88
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2022-02-13 16:00:22 -0800

    added version 3 support

--- src/V2/RoombaController.ts
+++ src/V2/RoombaController.ts
@@ -127,12 +127,7 @@ export class RoombaV2 {
   }
 
   async getMission(): Promise<MissionV2> {
-    const mission: MissionV2 = {
-      cycle: 'none',
-      phase: 'charge',
-      batPct: 99,
-      binPresent: true,
-      binFull: false };
+    let mission: MissionV2;
     return new Promise((resolve, reject) => {
       this.connect().then(async (roomba) => {
         roomba.getRobotState(['cleanMissionStatus', 'bin', 'batPct'])
@@ -149,3 +144,105 @@ export interface MissionV2 {
   binPresent: boolean;
   binFull: boolean;
 }
+export class RoombaV3 {
+  private connected = false;
+  public roomba?: dorita980;
+
+  constructor(private readonly blid: string, private readonly password: string, private readonly ip: string) {
+    //super(blid, password, ip, 2);
+  }
+
+  connect(): Promise<dorita980> {
+    return new Promise((resolve) => {
+      if (!this.connected) {
+        this.roomba = new dorita980(this.blid, this.password, this.ip, 2);
+        this.roomba.on('offline', () => {
+          this.connected = false;
+          this.roomba.end();
+        }).on('close', () => {
+          this.connected = false;
+        }).on('connect', () => {
+          this.connected = true;
+          resolve(this.roomba);
+        });
+      } else {
+        resolve(this.roomba);
+      }
+    });
+  }
+
+  clean() {
+    this.connect().then(async (roomba) => {
+      await roomba.clean();
+      await roomba.end();
+    });
+  }
+
+  pause() {
+    this.connect().then(async (roomba) => {
+      await roomba.pause();
+      await roomba.end();
+    });
+  }
+
+  stop() {
+    this.connect().then(async (roomba) => {
+      await roomba.stop();
+      await roomba.end();
+    });
+  }
+
+  resume() {
+    this.connect().then(async (roomba) => {
+      await roomba.resume();
+      await roomba.end();
+    });
+  }
+
+  dock() {
+    this.connect().then(async (roomba) => {
+      await roomba.dock();
+      await roomba.end();
+    });
+  }
+
+  find() {
+    this.connect().then(async (roomba) => {
+      await roomba.find();
+      await roomba.end();
+    });
+  }
+
+  async getMission(): Promise<MissionV3> {
+    let mission: MissionV3;
+    return new Promise((resolve, reject) => {
+      this.connect().then(async (roomba) => {
+        roomba.getRobotState(['cleanMissionStatus', 'bin', 'batPct'])
+          .then(state => resolve(Object.assign(mission, state.cleanMissionStatus, state.lastCommand, state.bin, state)))
+          .catch(err => reject(err));
+      });
+    });
+  }
+}
+export interface MissionV3 {
+  cycle: 'none' | 'clean';
+  phase: 'charge' | 'stuck' | 'run' | 'hmUsrDock';
+  batPct: number;
+  binPresent: boolean;
+  binFull: boolean;
+  pmap_id: string | null;
+  regions: [{
+    region_id: string; type: 'rid' | 'zid';},
+  ] | null;
+  user_pmapv_id: string | null;
+}
+export interface Map {
+  ordered: 1;
+  pmap_id: string;
+  regions: [
+    { region_id: string; type: 'rid' | 'zid'},
+  ];
+  user_pmapv_id: string;
+}
+
+
--- src/V2/platformAccessory.ts
+++ src/V2/platformAccessory.ts
@@ -1,6 +1,6 @@
 import { Service, PlatformAccessory } from 'homebridge';
 import { iRobotPlatform } from './platform';
-import { RoombaV1, RoombaV2, MissionV1, MissionV2 } from './RoombaController';
+import { RoombaV1, RoombaV2, MissionV1, MissionV2, MissionV3, RoombaV3 } from './RoombaController';
 import { EventEmitter } from 'events';
 
 const events = new EventEmitter();
@@ -78,6 +78,7 @@ export class iRobotPlatformAccessoryV1 {
       const sensorType: 'contact' | 'motion' | 'filter' = sensor.type;
       const value = (mission: MissionV1) => {
         const conditions = sensor.codition.split[':'];
+        conditions[1] = conditions[1]==='true' ? true : conditions[1] === 'false' ? false : conditions[1];
         if (conditions[0] === 'inverted') {
           return mission.ok[conditions[1]] !== conditions[2];
         } else {
@@ -143,7 +144,7 @@ export class iRobotPlatformAccessoryV2 {
     phase: 'charge',
     batPct: 99,
     binPresent: true,
-    binFull: false
+    binFull: false,
   };
 
   constructor(
@@ -157,7 +158,8 @@ export class iRobotPlatformAccessoryV2 {
       .setCharacteristic(this.platform.Characteristic.SerialNumber, this.accessory.context.device.serialNum || this.accessory.UUID || 'N/A')
       .setCharacteristic(this.platform.Characteristic.FirmwareRevision, 2)
       .getCharacteristic(this.platform.Characteristic.Identify).on('set', () => {
-        this.platform.log.error(this.logPrefix, 'Identification not supported');
+        this.platform.log.error(this.logPrefix, 'Identifying... (Might not work if device is docked)');
+        this.roomba.find();
       });
 
 
@@ -200,6 +202,134 @@ export class iRobotPlatformAccessoryV2 {
       const sensorType: 'contact' | 'motion' | 'filter' = sensor.type;
       const value = (mission: MissionV2) => {
         const conditions = sensor.codition.split[':'];
+        conditions[1] = conditions[1]==='true' ? true : conditions[1] === 'false' ? false : conditions[1];
+        if (conditions[0] === 'inverted') {
+          return mission[conditions[1]] !== conditions[2];
+        } else {
+          return mission[conditions[0]] === conditions[1];
+        }
+      };
+      if (sensorType === 'contact') {
+        const contact = this.accessory.getService(this.accessory.displayName + ' ' + sensor.condition) ||
+          this.accessory.addService(this.platform.Service.ContactSensor, this.accessory.displayName + ' ' + sensor.condition,
+            'Contact-' + sensor.condition);
+        contact.getCharacteristic(this.platform.Characteristic.ContactSensorState)
+          .onGet(() => {
+            return value(this.state) ? 1 : 0;
+          });
+        events.on('update', (mission) => {
+          contact.updateCharacteristic(this.platform.Characteristic.ContactSensorState, value(mission) ? 1 : 0);
+        });
+
+      } else if (sensorType === 'motion') {
+        const motion = this.accessory.getService(this.accessory.displayName + ' ' + sensor.condition) ||
+          this.accessory.addService(this.platform.Service.MotionSensor, this.accessory.displayName + ' ' + sensor.condition,
+            'Motion-' + sensor.condition);
+        motion.getCharacteristic(this.platform.Characteristic.MotionDetected)
+          .onGet(() => {
+            return value(this.state);
+          });
+        events.on('update', (mission) => {
+          motion.updateCharacteristic(this.platform.Characteristic.ContactSensorState, value(mission));
+        });
+
+      } else if (sensorType === 'filter') {
+        const filter = this.accessory.getService(this.accessory.displayName + ' ' + sensor.condition) ||
+          this.accessory.addService(this.platform.Service.FilterMaintenance, this.accessory.displayName + ' ' + sensor.condition,
+            'Filter-' + sensor.condition);
+        filter.getCharacteristic(this.platform.Characteristic.FilterChangeIndication)
+          .onGet(() => {
+            return value(this.state) ? 1 : 0;
+          });
+        events.on('update', (mission) => {
+          filter.updateCharacteristic(this.platform.Characteristic.ContactSensorState, value(mission) ? 1 : 0);
+        });
+      }
+    }
+    const interval = setInterval(() => {
+      this.roomba.getMission()
+        .then(mission => events.emit('update', mission))
+        .catch(err => this.platform.log.error(this.logPrefix, 'Failed To Update State:\n', err));
+    }, this.platform.config.interval || 5000);
+    this.platform.api.on('shutdown', () => {
+      clearInterval(interval);
+    });
+  }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+export class iRobotPlatformAccessoryV3 {
+  private service: Service;
+  private logPrefix = '[${this.accessory.context.displayName}]';
+  private roomba = new RoombaV3(this.accessory.context.blid, this.accessory.context.password, this.accessory.context.ip);
+  private state: MissionV3 = {
+    cycle: 'none',
+    phase: 'charge',
+    batPct: 99,
+    binPresent: true,
+    binFull: false,
+    pmap_id: null,
+    regions: null,
+    user_pmapv_id: null,
+  };
+
+  constructor(
+    private readonly platform: iRobotPlatform,
+    private readonly accessory: PlatformAccessory,
+  ) {
+    // set accessory information
+    this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'iRobot')
+      .setCharacteristic(this.platform.Characteristic.Model, this.accessory.context.device.sku || 'N/A')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, this.accessory.context.device.serialNum || this.accessory.UUID || 'N/A')
+      .setCharacteristic(this.platform.Characteristic.FirmwareRevision, 2)
+      .getCharacteristic(this.platform.Characteristic.Identify).on('set', () => {
+        this.platform.log.error(this.logPrefix, 'Identifying... (Might not work if device is docked)');
+        this.roomba.find();
+      });
+
+
+    this.service = this.accessory.getService(this.accessory.context.device.name) ||
+      this.accessory.addService(this.platform.Service.Fan, this.accessory.context.device.name, 'Main-Service');
+
+    this.service.setPrimaryService(true);
+
+
+    this.service.getCharacteristic(this.platform.Characteristic.On)
+      .onSet((value) => {
+        const stopActions = this.platform.config.offAction !== undefined ? this.platform.config.offAction.split(':') : ['pause', 'dock'];
+        if (value as boolean) {
+          this.roomba.clean();
+          this.roomba.resume();
+        } else {
+          this.roomba[stopActions[0]];
+          setTimeout(() => {
+            if (stopActions[1] !== 'none') {
+              this.roomba[stopActions[1]];
+            }
+          }, 5000);
+        }
+      }).onGet(() => {
+        const status = this.platform.config.status !== undefined ? this.platform.config.status.split(':') : ['phase', 'run'];
+        return new Promise((resolve, reject) => {
+          this.roomba.getMission().then(mission => {
+            this.state = mission;
+            events.emit('update', mission);
+            resolve(status[0] === 'inverted' ? mission[status[1]] !== status[2] : mission[status[0]] === status[1]);
+          }).catch(err => reject(err));
+        });
+      });
+    events.on('update', (mission) => {
+      const status = this.platform.config.status !== undefined ? this.platform.config.status.split(':') : ['phase', 'run'];
+      this.service.updateCharacteristic(this.platform.Characteristic.On,
+        status[0] === 'inverted' ? mission.ok[status[1]] !== status[2] : mission.ok[status[0]] === status[1]);
+    });
+    for (const sensor of this.platform.config.sensors) {
+      const sensorType: 'contact' | 'motion' | 'filter' = sensor.type;
+      const value = (mission: MissionV3) => {
+        const conditions = sensor.codition.split[':'];
+        conditions[1] = conditions[1]==='true' ? true : conditions[1] === 'false' ? false : conditions[1];
         if (conditions[0] === 'inverted') {
           return mission[conditions[1]] !== conditions[2];
         } else {
