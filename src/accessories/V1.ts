0901c4a39183ab3fcb313cdcd48d3e88eec92c4e
Kolton Bloom <bloomkd46@icloud.com>
2022-12-27 15:39:06 -0800

    added v1 support (untested)

---
+++ src/accessories/V1.ts
@@ -0,0 +1,253 @@
+import { lookup } from 'dns/promises';
+import { CharacteristicChange, CharacteristicValue, PlatformAccessory } from 'homebridge';
+import ping from 'ping';
+
+import { getRobotByBlid, Local, LocalV1 } from '@bloomkd46/dorita980';
+
+import { iRobotPlatform } from '../platform';
+import { Context, Device, V1Mission } from '../settings';
+import Accessory, { ActiveIdentifier, ActiveIdentifierPretty } from './Accessory';
+
+
+export default class V1Roomba extends Accessory {
+  public _lastKnownState = (this.accessory.context.lastState as Partial<V1Mission> | undefined) ?? {};
+  private recentlySet = false;
+  public set lastKnownState(state: Partial<V1Mission>) {
+    this.accessory.context.lastState = state;
+    if (!this.recentlySet) {
+      this._lastKnownState = state;
+      this.update();
+    }
+  }
+
+  public get lastKnownState() {
+    return this._lastKnownState;
+  }
+
+  public mode = 0;
+  public ip?: string = this.accessory.context.ip;
+  private offline = false;
+  private keepAlive = false;
+  dorita980?: LocalV1.Local;
+  update() {
+    this.service.updateCharacteristic(this.platform.Characteristic.Active, this.offline ? 1 : this.keepAlive ? 1 : 0);
+    this.service.updateCharacteristic(this.platform.Characteristic.ActiveIdentifier, this.getActivity());
+    if (this.platform.config.alwaysShowModes !== true) {
+      this.updateVisibility(this.getActivity());
+    }
+  }
+
+  constructor(
+    private readonly platform: iRobotPlatform,
+    private readonly accessory: PlatformAccessory<Context>,
+    private readonly device: Device,
+  ) {
+    super(platform, accessory, device, accessory.getService(platform.Service.Television) ||
+      accessory.addService(platform.Service.Television));
+    if (this.accessory.context.emptyCapable) {
+      this.addEmptyBinService();
+    }
+    this.accessory.getService(platform.Service.AccessoryInformation)!.getCharacteristic(platform.Characteristic.Identify)
+      .on('set', async () => {
+        this.log('info', 'Finding Device:', this.lastKnownState);
+      });
+
+    // handle on / off events using the Active characteristic
+    //ACTIVE: 1; INACTIVE: 0;
+    this.service.getCharacteristic(this.platform.Characteristic.Active)
+      .onSet(async value => {
+        if (value === 0) {
+          this.keepAlive = false;
+        } else if (value === 1) {
+          this.keepAlive = true;
+          await this.connect();
+        }
+      }).onGet(() => this.offline ? 0 : this.keepAlive ? 1 : 0);
+    this.service.setCharacteristic(this.platform.Characteristic.Active, (this.platform.config.autoConnect ?? true) ? 1 : 0);
+    this.service.setCharacteristic(this.platform.Characteristic.ActiveIdentifier, ActiveIdentifier.Docked);
+    this.service.getCharacteristic(this.platform.Characteristic.ActiveIdentifier)
+      .onSet(this.setActivity.bind(this)).onGet(this.getActivity.bind(this)).on('change', this.notifyActivity.bind(this));
+  }
+
+  private interval?: NodeJS.Timer;
+  connect(): Promise<LocalV1.Local> {
+    return new Promise((resolve, reject) => {
+      if (this.dorita980) {
+        resolve(this.dorita980);
+      } else {
+        this.getIp().then(ip => {
+          this.dorita980 = Local(this.device.blid, this.device.password, ip, 1);
+          this.interval = setInterval(async () => {
+            if (this.keepAlive) {
+              const state = (await this.dorita980?.getMission().catch(() => {
+                this.offline = true; /*this.ip = undefined;*/ this.dorita980 = undefined;
+              }))?.ok ?? this.log('warn', 'Failed to fetch state');
+              if (state) {
+                const oldState = this.lastKnownState;
+                this.lastKnownState = Object.assign(oldState, state);
+              }
+            } else {
+              if (this.interval) {
+                clearInterval(this.interval); this.interval = undefined;
+              }
+            }
+          }, 500);
+          resolve(this.dorita980);
+        }).catch(() => {
+          this.log('warn', 'Offline'); reject();
+        });
+      }
+    });
+  }
+
+  async getIp(): Promise<string> {
+    try {
+      if (this.ip) {
+        return await new Promise((resolve, reject) =>
+          ping.promise.probe(this.ip!)
+            .then(data => resolve(data.numeric_host ?? reject() ?? '')).catch(err => reject(err)));
+      }
+      let ip = '';
+      switch (this.device.ipResolution) {
+        case 'broadcast':
+          ip = await new Promise((resolve, reject) =>
+            getRobotByBlid(this.device.blid, (err, data) => reject(err || resolve(data.ip))));
+          break;
+        case 'lookup':
+          ip = await new Promise((resolve, reject) =>
+            lookup(this.device.publicInfo.hostname + '.local', 4).then(data => resolve(data.address)).catch(err => reject(err)));
+          break;
+        default:
+          ip = await new Promise((resolve, reject) =>
+            ping.promise.probe('ip' in this.device ? this.device.ip : this.device.publicInfo.ip)
+              .then(data => resolve(data.numeric_host ?? reject() ?? '')).catch(err => reject(err)));
+          break;
+      }
+      if (ip !== this.ip) {
+        this.accessory.context.ip = ip;
+        this.log(2, `Updating IP Address To ${ip}`);
+      }
+      return ip;
+    } catch (err) {
+      this.log('warn', err as string);
+      throw err;
+    }
+  }
+
+  async setActivity(activeValue: CharacteristicValue) {
+    this.recentlySet = true;
+    //this.log(4, `setActivity: ${activeValue}`);
+    const value = activeValue as ActiveIdentifier;
+    await this.connect();
+    switch (value) {
+      case ActiveIdentifier.Clean_Everywhere:
+        await this.dorita980?.start() ?? this.log('warn', 'Failed to clean');
+        (() => {
+          const oldState = this.lastKnownState;
+          this.lastKnownState = Object.assign(oldState, { cycle: 'clean', phase: 'run' });
+        })();
+        break;
+      case ActiveIdentifier.Pause:
+        await this.dorita980?.pause() ??
+          this.log('warn', 'Failed to pause');
+        (() => {
+          const oldState = this.lastKnownState;
+          this.lastKnownState = Object.assign(oldState, { cycle: 'clean', phase: 'stop' });
+        })();
+        break;
+      case ActiveIdentifier.Off:
+        if (this.lastKnownState.phase !== 'charge') {
+          await new Promise(resolve => {
+            let docked = false;
+            setInterval(async () => {
+              if (this.lastKnownState.phase === 'stop' && !docked) {
+                docked = true;
+                resolve(await this.dorita980?.dock() ??
+                  this.log('warn', 'Failed to dock'));
+                (() => {
+                  const oldState = this.lastKnownState;
+                  this.lastKnownState = Object.assign(oldState, { cycle: 'clean', phase: 'hmUsrDock' });
+                })();
+              }
+            }, 500);
+            this.dorita980?.pause() ??
+              this.log('warn', 'Failed to pause');
+            (() => {
+              const oldState = this.lastKnownState;
+              this.lastKnownState = Object.assign(oldState, { cycle: 'clean', phase: 'stop' });
+            })();
+          });
+        }
+        break;
+      default:
+        break;
+    }
+    setTimeout(() => {
+      this.recentlySet = false;
+    }, 500);
+  }
+
+  getActivity(): ActiveIdentifier {
+    switch (this.lastKnownState.phase) {
+      case 'recharge':
+      case 'cancelled':
+      case '':
+      case undefined:
+        return ActiveIdentifier.Docked;
+      case 'hmUsrDock':
+      case 'dock':
+      case 'dockend':
+      case 'hmPostMsn':
+      case 'hmMidMsn':
+        return ActiveIdentifier.Docking;
+      case 'new':
+      case 'run':
+      case 'resume':
+        switch (this.lastKnownState.cycle) {
+          case 'evac':
+            if (!this.accessory.context.emptyCapable) {
+              this.log(4, 'Adding Bin Empty Service');
+              this.addEmptyBinService();
+            }
+            return ActiveIdentifier.Emptying_Bin;
+          default:
+            return ActiveIdentifier.Cleaning_Everywhere;
+        }
+      case 'pause':
+        return ActiveIdentifier.Paused;
+      case 'stop':
+      case 'charge':
+        switch (this.lastKnownState.cycle) {
+          case 'none':
+            return ActiveIdentifier.Docked;
+          case 'evac':
+            return ActiveIdentifier.Emptying_Bin;
+          default:
+            return ActiveIdentifier.Paused;
+        }
+      case 'stuck':
+        //this.log('warn', 'Stuck!');
+        return ActiveIdentifier.Stuck;
+      case 'evac':
+        if (!this.accessory.context.emptyCapable) {
+          this.log(4, 'Adding Bin Empty Service');
+          this.addEmptyBinService();
+        }
+        return ActiveIdentifier.Emptying_Bin;
+      default:
+        //Add unknown channel?
+        this.log('warn', 'Unknown phase:', this.lastKnownState.phase);
+        return ActiveIdentifier.Docked;
+    }
+  }
+
+  async notifyActivity(value: CharacteristicChange) {
+    if (value.newValue !== value.oldValue) {
+      const status = ActiveIdentifierPretty[value.newValue as number];
+      if (status) {
+        this.log(status === 'Stuck' ? 'warn' : 3, this.accessory.context.overrides[value.newValue as number] || status);
+      }
+      //this.log(4, `${this.lastKnownState.cleanMissionStatus?.cycle} : ${this.lastKnownState.cleanMissionStatus?.phase}`);
+    }
+  }
+}
