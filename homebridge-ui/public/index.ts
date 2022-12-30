3fd6595ddfed194af9a4eb8279de495e25db1e29
Kolton Bloom <bloomkd46@icloud.com>
2022-12-30 13:53:08 -0800

    fixed config error

--- homebridge-ui/public/index.ts
+++ homebridge-ui/public/index.ts
@@ -198,6 +198,7 @@ const menuWrapper = document.getElementById('menuWrapper') as HTMLDivElement;
             console.log(devices);
             homebridge.toast.success(`Successfully Configured ${devices.length} Devices`, 'Success!');
             const config = await homebridge.getPluginConfig() as Config[];
+            config[0].accessories = config[0].accessories || [];
             console.debug(config);
             for (const device of devices) {
               if (device.ipResolution === 'manual' && !device.ip) {
